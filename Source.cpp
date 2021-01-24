ed31c4d7ef7b2b1020b3af79be8629bed62aee17
Ege Tuna Diken <egetuna.diken@gmail.com>
2021-01-24 16:37:37 +0300

    another version

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <map>
 
 using namespace std;
 
@@ -68,19 +69,43 @@ void DeleteNodeFromTree(Node*& pRoot, int i)
     delete q;
 }
 
+void BranchCreator(Node* pRoot, vector<int> branch, vector<vector<int> >& vect)
+{
+    branch.push_back(pRoot->i);//adding this to the branch
+    
+    if(!(pRoot->pLeft) && !(pRoot -> pRight)){vect.push_back(branch);} //if they're both empty, this is done.
+    else if (pRoot->pLeft && !(pRoot->pRight)){BranchCreator(pRoot->pLeft, branch, vect);} //if only there's a left branch
+    else if (pRoot->pRight && !(pRoot->pLeft)){BranchCreator(pRoot->pRight, branch, vect);} //if there's only a right branch
+    else {BranchCreator(pRoot->pLeft, branch, vect); BranchCreator(pRoot->pRight, branch, vect);} //if there are two branches to either side.
+}
+
 
 void Checker(Node* pRoot) 
 {
-    Node* root = pRoot;
-    vector <vector<int> > vect;
+    vector<vector<int> > vect;
     vector <int> branch;
-    while(root)
-    {
-        branch.push_back(pRoot->i);
-        if(!(pRoot->pLeft || pRoot -> pRight)){vect.push_back(branch); break;}
 
+    BranchCreator(pRoot, branch, vect); //this creates all of the branches and stores them in vect as seperate branches.
+
+    int sum = 0;// this will give out the sum
+    int b = -1; //this will determine which branch in vect is the greatest branch
+    for (int i = 0 ; i < vect.size() ; i++)
+    {
+        int insum = 0; //this is for comparison purposes.
+        for (int j = 0 ; j < vect[i].size() ; j++)
+        {
+            insum += vect[i][j]; 
+        }
+        if (insum > sum){sum = insum; b=i;} //checked if bigger than previous sum, if so, replaced.
     }
+
     cout << "Branch with the largest sum is: ";
+    
+    for (int j = 0 ; j < vect[b].size() ; j++)
+    {
+        cout << vect[b][j] << " ";
+    }
+    cout << "-> SUM = " << sum;
 }
 
 int main()
